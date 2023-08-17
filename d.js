6da5da726f4fc08cb2712855aba6c43e44b57e87
Zeman <zemanapp@163.com>
2023-08-17 16:38:27 +0800

    新功能01完成

--- d.js
+++ d.js
@@ -2,9 +2,8 @@
  * @Author: Zeman zemanapp@163.com
  * @Date: 2023-08-17
  * @LastEditors: Zeman zemanapp@163.com
- * @Description: future
+ * @Description:新功能01
  * Copyright (c) 2023 by ${git_name_email}, All Rights Reserved.
  */
-let b;
-b = 3;
-console.log("b", b);
+let d;
+d = "hello world";
