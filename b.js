a114c23c45a26edc28cf010f2e33668f8e535d10
Zeman <zemanapp@163.com>
2023-08-17 16:20:44 +0800

    b

---
+++ b.js
@@ -0,0 +1,8 @@
+/*
+ * @Author: Zeman zemanapp@163.com
+ * @Date: 2023-08-17
+ * @LastEditors: Zeman zemanapp@163.com
+ * @Description: future
+ * Copyright (c) 2023 by ${git_name_email}, All Rights Reserved.
+ */
+let b = 2;
